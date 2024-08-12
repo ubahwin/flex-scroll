f5515674f746bf17b7b6f6ba00c03cd876b57c09
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-08-12 20:33:40 +0300

    fix(package): swift-tools-version 5.10

--- Package.swift
+++ Package.swift
@@ -1,19 +1,20 @@
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
     name: "flex-scroll",
     platforms: [
-        .iOS(.v15)
-    ]
+        .iOS(.v14)
+    ],
     products: [
         .library(
-            name: "flex-scroll",
-            targets: ["flex-scroll"]
+            name: "FlexScroll",
+            targets: ["FlexScroll"]
         )
-    ]
+    ],
     targets: [
         .target(
-            name: "flex-scroll"
+            name: "FlexScroll"
         ),
     ]
 )
